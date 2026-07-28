b581a41d4014b4355f9b7a198f48a3216def972e
BWGPit <pitoverkill@gmail.com>
2026-07-28 20:16:34 +0200

    Centering for desktop

--- Scripts/cards.js
+++ Scripts/cards.js
@@ -1,8 +1,6 @@
 import cardlist from '../Data/cardlist.json' with {type: "json"}
 let cd = document.getElementById("carddiv")
 
-console.log("TEST")
-
 let innerhtml = ""
 for (let c of cardlist) {
     innerhtml += `<div class="card-display"><img src="Data/CardImages/${c.card_id}.png" class="cardimg"><pre class="card-text"><p class="cardname">${c.name}</p>${c.effect}</pre></div>`
